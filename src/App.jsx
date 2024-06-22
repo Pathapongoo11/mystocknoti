542daf49acb950f9dbc201d5778f380da35c7fa6
pathapong2 <o@livingmobile.me>
2024-06-22 16:40:16 +0700

    Feature improve menu and create hook to call api

--- src/App.jsx
+++ src/App.jsx
@@ -2,112 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import Modal from "react-modal";
 import "./index.css";
+import PostDataLine from "./components/hook-service/hook-service-post";
+import ProudctProductsList from "./components/products/product-list";
 
 function ProductList() {
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      name: "ลูกชิ้นปลา",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 2,
-      name: "ลูกชิ้นหมู",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 3,
-      name: "ปลาเส้น",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 4,
-      name: "ลูกชิ้นวัว",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 5,
-      name: "ลูกชิ้นปลา",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 6,
-      name: "ลูกชิ้นหมู",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 7,
-      name: "ปลาเส้น",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 8,
-      name: "ลูกชิ้นวัว",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 9,
-      name: "ลูกชิ้นปลา",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 10,
-      name: "ลูกชิ้นหมู",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 11,
-      name: "ปลาเส้น",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-    {
-      id: 12,
-      name: "ลูกชิ้นวัว",
-      image:
-        "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/19/02/0212419/0212419.jpg",
-      isConfirmed: false,
-      quantity: 1,
-    },
-
-    // รายการสินค้าเพิ่มเติมตามความจำเป็น
-  ]);
+  const [products, setProducts] = useState(ProudctProductsList);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedProductIndex, setSelectedProductIndex] = useState(-1);
   const selectedProduct = products[selectedProductIndex];
   const isAnyProductConfirmed = products.some((product) => product.isConfirmed);
+  const { postData, loading, error } = PostDataLine();
 
   function openModal(index) {
     setSelectedProductIndex(index);
@@ -115,7 +19,6 @@ function ProductList() {
   }
 
   function closeModal() {
-    console.log(products[selectedProductIndex]);
     if (!products[selectedProductIndex].isConfirmed) {
       resetProduct(selectedProductIndex);
     }
@@ -128,7 +31,6 @@ function ProductList() {
         ? { ...product, isConfirmed: true }
         : product
     );
-    console.log(updatedProducts);
     closeModal();
     setProducts(updatedProducts);
   }
@@ -165,12 +67,42 @@ function ProductList() {
     setProducts(updatedProducts);
   }
 
+  function save() {
+    const confirmedProducts = products
+      .filter((p) => p.isConfirmed)
+      .map((p) => {
+        return {
+          item: p.isConfirmed ? "ของหมด" : "ไม่ขาด",
+          status: `${p.name} ${
+            p.isConfirmed == true && p.unit
+              ? `เอา ${p.quantity} ${p.unit} `
+              : `${p.quantity} จำนวน`
+          }`,
+        };
+      });
+
+    postData(confirmedProducts);
+    clearTiem();
+  }
+
+  function clearTiem() {
+    setProducts(
+      products.map((p) => ({ ...p, isConfirmed: false, quantity: 1, unit: "" }))
+    );
+    setModalIsOpen(false);
+  }
+
   return (
     <>
+      {loading && (
+        <div className="full-page-overlay">
+          <div className="spinner">{error && <p>เกิดข้อผิดพลาด</p>}</div>
+        </div>
+      )}
       <center>
         <p className="Header-Text">รายการสินค้าที่ต้องสั่ง</p>
       </center>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-3">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-3">
         {products.map((product, index) => (
           <div
             key={product.id}
@@ -185,7 +117,7 @@ function ProductList() {
               className="w-24 h-24 object-cover rounded-full mx-auto mb-3"
             />
             <div className="text-sm font-medium product-name">
-              {product.name}
+              {index + 1} . {product.name}
             </div>
           </div>
         ))}
@@ -198,8 +130,9 @@ function ProductList() {
               : "bg-gray-400 text-black"
           }`}
           disabled={!isAnyProductConfirmed}
+          onClick={() => save()}
         >
-          ยืนยัน
+          ส่งข้อมูล
         </button>
       </div>
       {selectedProduct && modalIsOpen && (
@@ -214,6 +147,14 @@ function ProductList() {
             <h2 className="text-lg font-semibold mb-4 product-item-name">
               {selectedProduct.name}
             </h2>
+            <div className="flex items-center mb-4">
+              <img
+                src={selectedProduct.image}
+                alt={selectedProduct.name}
+                className="w-24 h-24 object-cover rounded-full mx-auto mb-3"
+              />
+            </div>
+
             <div className="flex items-center mb-4">
               <label className="text-sm font-medium mr-3 text-black">
                 จำนวน
@@ -250,16 +191,25 @@ function ProductList() {
               >
                 <option value="">กรุณาเลือก</option>
                 <option value="กิโล">โล</option>
-                <option value="อัน">อัน</option>
+                <option value="ครึ่งโล">ครึ่งโล</option>
+                <option value="กรัม">กรัม (*สำหรับ ผักบุ้ง)</option>
                 <option value="ชิ้น">ชิ้น</option>
-                <option value="ถุง">ถุง</option>
+                <option value="ถุง">
+                  ถุง (*สำหรับ ลูกชิ้นกุ้ง, แคระ, ลูกชิ้นปลา , เลือดหม)
+                </option>
+                <option value="กล่อง">กล่อง (*สำหรับ มาม่า)</option>
+                <option value="ขีด">ขีด (*สำหรับรากผักชี)</option>
+                <option value="โหล">โหล (*สำหรับ น้ำกระเทียมดอง)</option>
+                <option value="ขวด">
+                  ขวด (*สำหรับ ซอสแห้ง , ซีอิ๋วขาว , ซอสฝาเขียว , น้ำมัน)
+                </option>
               </select>
             </div>
             <button
               onClick={confirmOut}
               className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
             >
-              ยืนยัน
+              ยินยัน
             </button>
           </div>
         </Modal>
---
+++ src/components/products/product-list.jsx
@@ -0,0 +1,246 @@
+function ProductsList() {
+
+ return [
+  {
+    name: "ลูกชิ้นกุ้ง",
+    image:
+      "https://mallika.co.th/wp-content/uploads/2020/04/ลูกชิ้นกุ้งล้วน-10-ลูก-01.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ลูกชิ้นแคระ",
+    image:
+      "https://s359.kapook.com/r/600/auto/pagebuilder/635d14ab-8974-4066-b189-1d66345575ea.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ปลาเส้น",
+    image:
+      "https://www.thaitechno.net/uploadedimages/c1/Product_44364_614310513_fullsize.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ลูกชิ้นหมู",
+    image:
+      "https://shop-image.readyplanet.com/6V-xuqiWpS-GbaLFK6GgC6Ro6HI=/05ed179d53d444929cf7f2f42fb337a9",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ฟ้าไทย",
+    image:
+      "https://assets.tops.co.th/FATHAI-FathaiPorkFlavoredSeasoningPowder165g-8854334002733-1",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ชูรส",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp2zV3AAFWwQIJazMHgI-A-zDHq6Y6rxjTgA&s",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ผักบุ้ง",
+    image: "https://inwfile.com/s-cr/dhemrx.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ถั่งงอก",
+    image:
+      "https://s.isanook.com/he/0/ud/3/17581/beansprout.jpg?ip/crop/w670h402/q80/jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ต้นหอม",
+    image:
+      "https://s.isanook.com/he/0/ud/0/4317/istock_95202947_medium.jpg?ip/crop/w670h402/q80/jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "รากผักชี",
+    image:
+      "https://res.cloudinary.com/freshketimage001/image/upload/v1507526805/flm0zb8kaozh6qbryq90.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "เลือดหมู",
+    image: "https://f.ptcdn.info/713/062/000/pnmnmue6xitZx1h2ILA-o.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "เส้นใหญ่",
+    image:
+      "https://s359.kapook.com/r/600/auto/pagebuilder/56b5d27d-0ad6-4dae-9f49-37012235aa0b.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "เส้นเล็ก",
+    image:
+      "https://lh6.googleusercontent.com/proxy/MEZ9a96L6bjFvHvTzy_tTO4GOvRU-nVDQbbU7EpkeVOViENa3gGZuf5_XmX3ddfuLZojxLli-5ZnJ7gPxafC4AnRMrEgmd5gVzbCu_rG_IltTzOCFAW8efT46WD_eazBqOI",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "เกี่ยมอี๋",
+    image:
+      "https://lh3.googleusercontent.com/proxy/9FVmZ3DKsuroMxKRJddGy3SXUpwWq54NwCp1ApiMnqqzd3VvbYPdQbQg4KkhwESXkYJh-xFuRjbW3btkoSwe42wvdWdDZTsPpM-P_xPUmp-W8xrA9CfnuAoCMhfCurEOqrU",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "วุ้นเส้น",
+    image:
+      "https://lh6.googleusercontent.com/proxy/xZWA6x7LOT8LchsUFb3nRrgH0-rVS_YCYsRwevpuCQuWF_3vBlrq95XGbtwSz-5Xo23xMLLoYb9eLgF8atV0WOl4sfVLzuHGoU8a7wKn3ySAeSjpUrt0juUqH2D7eb32gOA",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "บะหมี่",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Fresh_ramen_noodle_001.jpg/1200px-Fresh_ramen_noodle_001.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "หมีขาวไวๆ",
+    image:
+      "https://waiwai.co.th/uploads/images-cache/5052/filemanager/5bfd9669cfa255687f0a9b04191ebde5.png",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "มาม่า",
+    image:
+      "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/95/88/8850987128295/8850987128295_1_1.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ถุงดำ",
+    image:
+      "https://bangpleestationery.com/wp-content/uploads/2019/11/308829393-2.png",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "น้ำกระเทียมดอง",
+    image:
+      "https://putterfreshshop.com/wp-content/uploads/2022/10/น้ำกระเทียมดอง.png",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ซีอิ๋วขาว",
+    image:
+      "https://7space.sgp1.digitaloceanspaces.com/O085/1626929056.12dcfa703e905e6a88d86586f04a3258.png",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ซอสฝาเขียว",
+    image:
+      "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/35/88/8851954101235/8851954101235_1-20230118180151-.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ซอสโฟ",
+    image:
+      "https://i0.wp.com/www.raikaset.net/wp-content/uploads/2020/05/เย็นตาโฟ030-20200517.jpg?fit=700,400&ssl=1",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "พริกไทย",
+    image:
+      "https://apps.phar.ubu.ac.th/thaicrudedrug/userfiles/image/tcd091_Prigthailon/tcd91_5.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "กระเทียมหัว",
+    image:
+      "https://select.talaadthai.com/media/catalog/product/cache/696e3e68550c37c55175d3f8868a2dd8/f/g/fgv050019201_pp2qs21agaynxitn.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ซอสแห้ง",
+    image:
+      "https://down-th.img.susercontent.com/file/sg-50009109-6c2e0095a8813309bb96f142eedddf11_tn",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "น้ำตาลทราย",
+    image:
+      "https://www.prachachat.net/wp-content/uploads/2023/09/3-1-น้ำตาล-728x485.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "น้ำมะนาว",
+    image:
+      "https://shop-image.readyplanet.com/7ZEtL4c9bm4IesTtZ9lW7EAfge4=/5fb61b3a0d52414b85b15d938f0c89f7",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "น้ำตาลกลวด",
+    image: "https://sordaotieam.com/cdn/shop/files/500_20.jpg?v=1695379538",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "หมูสับ",
+    image: "https://www.pitchameat.com/wp-content/uploads/2023/08/2.webp",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "หมูชิ้น",
+    image:
+      "https://st.bigc-cs.com/cdn-cgi/image/format=webp,quality=90/public/media/catalog/product/58/02/0211158/0211158_1.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "กระดูกหมู",
+    image: "https://www.ตลาดหมูเอ็มเค.com/wp-content/uploads/2020/03/1097.png",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "ปลาหมึกกรอบ",
+    image:
+      "https://res.cloudinary.com/freshketimage001/image/upload/v1702463450/s0m4tbholnuwmtufpaxw.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "แมลงกระพุน",
+    image:
+      "https://www.hormchuy.com/backend/Upload/upload_files/287045058_5193117104134829_3427952924806734255_n.jpg",
+    isConfirmed: false,
+    quantity: 1,
+  },
+  {
+    name: "น้ำมันพืช",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjmutQfOAG_g6gt7GxEG6cpjXDARUqmYFB9Q&s",
+    isConfirmed: false,
+    quantity: 1,
+  },
+];
+}
+export default ProductsList;
